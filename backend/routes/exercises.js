2513fda89d5a747ee4ea85e450975ea92958cc0b
Michele T <michelet@Macintosh-2.homenet.telecomitalia.it>
2019-10-18 18:39:33 +0200

    mark now

---
+++ backend/routes/exercises.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+let Exercise = require('../models/exercise_model');
+
+
+//Get onload request
+router.route('/').get((req, res) => {
+  Exercise.find()
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Post request
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
+
+
+  const newExercise = new Exercise ({
+    username,
+    description,
+    duration,
+    date
+  });
+
+
+newExercise.save()
+  .then(() => res.json('Exercise added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 
-let User = require('../models/user.model');
+let User = require('../models/user_model');
 
+
+//Get onload request
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Post request
 router.route('/add').post((req, res) => {
   const username = req.body.username;
 
