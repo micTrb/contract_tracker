3fec7139ce9ac46ee2de28fa11ce0d618af152b3
Michele T <michelet@Macintosh-2.homenet.telecomitalia.it>
2019-10-19 11:33:22 +0200

    added get id and delete by id request in exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -31,4 +31,35 @@ newExercise.save()
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+
+//Get by id
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+//Delete by id
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted"))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
